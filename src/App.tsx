954fd3da95abd3bf25b9711b055d55a6dd63d3fd
Marek Hauzr <marek.hauzr@gmail.com>
2026-02-13 16:56:03 +0100

    single KOL report example

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,17 @@ import { ExamplesSection } from "./components/ExamplesSection";
 import { TeamSection } from "./components/TeamSection";
 import { CTASection } from "./components/CTASection";
 import { Footer } from "./components/Footer";
+import { KOLReportExamplePage } from "./components/KOLReportExamplePage";
+
 export function App() {
+  if (window.location.pathname === "/kol-marketing-performance") {
+    return <h1>Marketing Performance of KOLs</h1>;
+  }
+
+  if (window.location.pathname === "/KOL-report-example") {
+    return <KOLReportExamplePage />;
+  }
+
   return (
     <div className="flex flex-col min-h-screen bg-white">
       <Navbar />
